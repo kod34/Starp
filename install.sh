e9ca272761396956c9fd833d74a054051aef9753
kod34 <5od3a.yes@protonmail.com>
2022-05-06 10:52:45 -0400

    Update 1.1.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install macchanger xterm python3-pip -y
-sudo pip3 install prettytable scapy
+sudo pip3 install -r requirements.txt
 sudo cp $(readlink -f starp.py) ${PATH%%:*}/starp
